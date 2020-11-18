082b5fc1a2a430d2fe8e7fcaa9d7c24582e3166c
Tommy Treb <tomtrezb2003@gmail.com>
2020-11-18 16:29:11 -0600

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from hashlib import sha256
+
+x = 5
+y = 0
+
+while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
+    y += 1
+
+print(f'The solution is y = {y}')
+test = sha256(f'{x*y}'.encode()).hexdigest()
+print(test)
